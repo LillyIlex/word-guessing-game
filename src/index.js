221c2ea1be97a0fa5de72004ba9b294463cb484a
Lil Corris <lil@jbcole.co.uk>
2023-11-06 17:41:17 +0000

    Put data in its own file.

--- src/index.js
+++ src/index.js
@@ -48,68 +48,7 @@ let currentPlayerIndex = -1;
 let score = 0;
 let chosenPlayer = [];
 
-const singers = [
-  {
-    answer: "Beyonce",
-    clue1: "Crazy In Love",
-    clue2: "Single Ladies",
-    clue3: "If I Were A Boy",
-  },
-  {
-    answer: "Eminem",
-    clue1: "Chasing Pavements",
-    clue2: "Easy On Me",
-    clue3: "Stan",
-  },
-  {
-    answer: "Adele",
-    clue1: "Without Me",
-    clue2: "Mockingbird",
-    clue3: "Rolling In The Deep",
-  },
-  {
-    answer: "Queen",
-    clue1: "We Will Rock You",
-    clue2: "Find Me Somebody To Love",
-    clue3: "Bohemian Rhapsody",
-  },
-  {
-    answer: "Oasis",
-    clue1: "Cigarettes And Alcohol",
-    clue2: "Wonderwall",
-    clue3: "Don't Look Back In Anger",
-  },
-  {
-    answer: "Coldplay",
-    clue1: "Yellow",
-    clue2: "Paradise",
-    clue3: "Sky Full Of Stars",
-  },
-  {
-    answer: "Lizzo",
-    clue1: "About damn time",
-    clue2: "Good As Hell",
-    clue3: "Juice",
-  },
-  {
-    answer: "Blur",
-    clue1: "Coffee and TV ",
-    clue2: "Song 2",
-    clue3: "Parklife",
-  },
-  {
-    answer: "Lizzo",
-    clue1: "About damn time",
-    clue2: "Good As Hell",
-    clue3: "Juice",
-  },
-  {
-    answer: "Lizzo",
-    clue1: "About damn time",
-    clue2: "Good As Hell",
-    clue3: "Juice",
-  },
-];
+
 
 const getRandomPlayer = () => {
   return Math.floor(Math.random() * players);
